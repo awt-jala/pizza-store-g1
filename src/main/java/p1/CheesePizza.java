5c18f2e1b2cf07678d1f7f886ad0b3c0898d54cc
rrchcr8 <rrch.cr8@gmail.com>
2019-03-05 13:48:57 -0400

    ATTEMPT 2 TO FIX: [ant:checkstyle] [ERROR] /home/travis/build/awt-jala/pizza-store-g1/src/main/java/p1/CheesePizza.java:3: Missing a Javadoc comment. [JavadocType]

--- src/main/java/p1/CheesePizza.java
+++ src/main/java/p1/CheesePizza.java
@@ -1,11 +1,12 @@
 package main.java.p1;
 
 public class CheesePizza extends Pizza {
-//This is a concrete class from Pizza
-public CheesePizza(){
-	super(PizzaTypes.CHEESE);
-}
+	/*
+	 * This is a concrete class from Pizza
+	 */
 
-
+	public CheesePizza() {
+		super(PizzaTypes.CHEESE);
+	}
 
 }
