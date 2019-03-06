3efb75df1214237157947345d6955f4b743bdc97
rrchcr8 <rrch.cr8@gmail.com>
2019-03-05 23:58:20 -0400

    javadoc generated with the option Project> Generate javadoc

--- src/main/java/p1/CheesePizza.java
+++ src/main/java/p1/CheesePizza.java
@@ -1,11 +1,16 @@
 package main.java.p1;
-
+/**
+ * This is a concrete class of superClass Pizza
+ * @author Carlos Richter
+ * @version 1.0
+ */
 public class CheesePizza extends Pizza {
-	/*
-	 * This is a concrete class from Pizza
-	 */
+	
 
 	public CheesePizza() {
+		/**
+		 *Here is a constructor class Pizza 
+		 */
 		super(PizzaTypes.CHEESE);
 	}
 
