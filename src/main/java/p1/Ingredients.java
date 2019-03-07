e9a847f69b3d12d99a09221d35247f1c1cf61ee7
rrchcr8 <rrch.cr8@gmail.com>
2019-03-07 01:42:30 -0400

    x2

--- src/main/java/p1/Ingredients.java
+++ src/main/java/p1/Ingredients.java
@@ -5,29 +5,33 @@ public class Ingredients {
 	IngredientsType ingredientsType;
 	double unitcostAditionalIngredient;
 
-	public IngredientsType getIngredientsType() {
-		return ingredientsType;
+	public double getCostAditionalIngredient(IngredientsType ingredientsType) {
+		switch (ingredientsType) {
+		case ASPARAGUS:
+			return 1.1;
+		case ANCHOES:
+			return 2.1;
+		case BROCCOLI:
+			return 3.1;
+		case MOZARELLA_CHEESE:
+			return 3.0;
+		case GREEKCHEESE:
+			return 2.1;
+
+		default:
+			break;
+		}
+
+		return 0.0;
+
 	}
 
-	public double getCostAditionalIngredient(IngredientsType ingredientsType) {
-	switch(ingredientsType) {
-	case ASPARAGUS:  return 1.1; 
-	case ANCHOES: return 2.1;	
-	case BROCCOLI: return 3.1;	
-	case MOZARELLA_CHEESE: return 3.0;
-	case GREEKCHEESE: return 2.1;
-	
-	default: break;
-	}
-	
-	return 0.0; 
-		
+	public IngredientsType getIngredientsType() {
+		return ingredientsType;
 	}
 
 	public void setIngredientsType(IngredientsType ingredientsType) {
 		this.ingredientsType = ingredientsType;
 	}
-
-	
 
 }
