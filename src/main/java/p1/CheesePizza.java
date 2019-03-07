8dd76fcbe2975592988bdcf3ee6095d15ef9047e
Admin <Admin@10.28.136.3>
2019-03-07 19:09:57 -0400

    Fixing  Line has trailing spaces. [RegexpSingleline]  File contains tab characters (this is the first instance). [FileTabCharacter]  issing a Javadoc comment. [JavadocMethod]  is a magic number. [MagicNumber]

--- src/main/java/p1/CheesePizza.java
+++ src/main/java/p1/CheesePizza.java
@@ -4,16 +4,19 @@ import java.util.HashSet;
 
 /**
  * This is a concrete class of superClass Pizza.
- * 
  * @author CarlosRichter.
  * @version 1.0.
  */
 public class CheesePizza extends Pizza {
 	//public double totalCost;
-
+	public final double PIZZA_COST_WI =10;
 	public CheesePizza() {
 		super(PizzaTypes.CHEESE);
-		PIZZA_COST = 10;
+		/**
+		 * This is a constructor of the CheesePizza class.
+		 * below we can find the price of a pizza without extra ingredients.
+		 */
+		PIZZA_COST = PIZZA_COST_WI;
 
 	}
 
